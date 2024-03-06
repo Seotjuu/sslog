aa54bdc5bca589f1c4d43a89fe33088309d00cae
seotjuu <sjlee@moasoftware.co.kr>
2024-03-06 17:53:45 +0900

    [Fix] gh-pages setting

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/sslog",
+  output: "export",
   images: {
     unoptimized: true, // 이미지 정상적으로 불러올 수 있도록함
   },
+
 };
 
 export default nextConfig;
