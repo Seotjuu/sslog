b6e7ba78052d14fc87064177f250ca78a0a32292
oculi_s <oculis0925@yonsei.ac.kr>
2024-03-06 22:05:47 +0900

    [Fix] gh-pages fix learn english

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
+const debug = process.env.NODE_ENV !== "production";
 const nextConfig = {
   output: "export",
+  assetPrefix: !debug ? '/sslog/' : '',
   images: {
     unoptimized: true, // 이미지 정상적으로 불러올 수 있도록함
   },
