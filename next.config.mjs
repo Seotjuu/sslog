2b2e45bb7972d5866d0fe3dcd6e99507574f474b
seotjuu <tjrenffl8@naver.com>
2024-03-06 23:26:24 +0900

    [Fix] gh-pages tailwind 적용 진행중

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
-const debug = process.env.NODE_ENV !== "production";
 const nextConfig = {
   output: "export",
-  assetPrefix: !debug ? '/sslog/' : '',
-  images: {
-    unoptimized: true, // 이미지 정상적으로 불러올 수 있도록함
-  },
+  assetPrefix: '/sslog/',
   trailingSlash: true,
 };
 
