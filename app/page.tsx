323f6bdb55e0e4dd801e3d94e5584a07c8220a55
Seotjuu <sjlee@moasoftware.co.kr>
2024-03-08 16:17:59 +0900

    [Fix] deploy error complete

--- app/page.tsx
+++ app/page.tsx
@@ -16,9 +16,6 @@ const Navbar = () => {
         <Button>
           <Link href={"/signup"}>회원가입</Link>
         </Button>
-        <Button>
-          <Link href={"/"}>home</Link>
-        </Button>
       </div>
     </div>
   );
