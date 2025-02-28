6893b6fb765eef4917c689f6c57cbd79dd677474
Seotjuu <sjlee@moasoftware.co.kr>
2025-02-28 11:12:37 +0900

    [Fix] ENV

--- auth.config.ts
+++ auth.config.ts
@@ -2,7 +2,7 @@ import { NextAuthConfig } from "next-auth";
 
 export const authConfig: NextAuthConfig = {
   providers: [],
-  secret: process.env.AUTH_SECRET, // 필수
+  secret: process.env.NEXTAUTH_SECRET, // 필수
   pages: {
     signIn: "/login", // 커스텀 로그인 페이지
   },
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const debug = process.env.NODE_ENV !== "production";
+
 const nextConfig = {
   output: "standalone",
   trailingSlash: true,
