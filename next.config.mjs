f4e51893001bcd01ede0e5f5bbad326696d3c0cf
Seotjuu <sjlee@moasoftware.co.kr>
2025-02-28 10:18:25 +0900

    [Fix] Deploy config fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const debug = process.env.NODE_ENV !== "production";
 const nextConfig = {
-  output: "export",
   assetPrefix: !debug ? '/sslog/' : '',
   trailingSlash: true,
 };
+
+
+module.exports = nextConfig;
 
 export default nextConfig;
